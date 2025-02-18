9528a94e35d4397ba14a771f04bdf2b27c44e1d6
ivanhomziak <ivan.homziak@gmail.com>
2025-02-18 13:52:10 +0100

    added AOP

--- src/main/java/com/ihomziak/bankingapp/transactionms/TransactionManagementServiceApplication.java
+++ src/main/java/com/ihomziak/bankingapp/transactionms/TransactionManagementServiceApplication.java
@@ -3,15 +3,16 @@ package com.ihomziak.bankingapp.transactionms;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableDiscoveryClient
+@EnableAspectJAutoProxy
 public class TransactionManagementServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TransactionManagementServiceApplication.class, args);
     }
-
 }
